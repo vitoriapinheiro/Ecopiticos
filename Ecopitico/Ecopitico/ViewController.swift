6cd39152d485e0f2e57f0e025c82d8510c0a9694
vitoriapinheiro <vps3@cin.ufpe.br>
2022-09-12 17:54:10 -0300

    added TabBar

--- Ecopitico/Ecopitico/ViewController.swift
+++ Ecopitico/Ecopitico/ViewController.swift
@@ -7,11 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MyTabBarController: UITabBarController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupTabBarController()
+    }
+    private func setupTabBarController(){
+        let home = UINavigationController(rootViewController: Home())
+        let search = UINavigationController(rootViewController: Search())
+        let profile = UINavigationController(rootViewController: Profile())
+        
+        self.setViewControllers([home, search, profile], animated: false)
+        self.tabBar.backgroundColor = .white
+        self.tabBar.isTranslucent = false
+        
+        guard let items = tabBar.items else{return}
+                
+        items[0].image = UIImage(systemName: "house")
+        items[0].title = "Início"
+        items[1].image = UIImage(systemName: "magnifyingglass")
+        items[1].title = "Explorar"
+        items[2].image = UIImage(systemName: "person")
+        items[2].title = "Perfil"
+        self.tabBar.tintColor = UIColor(named: "EcoGreen")
+    }
+    
+}
+
+class Home: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .green
+    }
+
+
+    
+}
+
+class Search: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+    
+}
+
+class Profile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
     }
 
 
@@ -26,8 +75,8 @@ import SwiftUI
 struct View_Preview: PreviewProvider {
     static var previews: some View {
         Group {
-            ViewController().showPreview().previewDevice("iPhone 13")
-            ViewController().showPreview().previewDevice("iPhone 13").previewInterfaceOrientation(.landscapeLeft)
+            MyTabBarController().showPreview().previewDevice("iPhone 13")
+            MyTabBarController().showPreview().previewDevice("iPhone 13").previewInterfaceOrientation(.landscapeLeft)
         }
     }
 }
